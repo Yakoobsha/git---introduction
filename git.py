0f0e44d0b5b85b2812b45a52cf3df8a47feef859
Muhammed yakoobsham <yakoobsham642@gmail.com>
2022-01-19 21:19:50 +0530

    Intaial commit

--- git.py
+++ git.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+import random 
+lower = "abcdefghijklmnopqurstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQURSTUVWXYZ"
+NUMBER = "0123456789"
+Symbol = "[ ]{ }# @ ( ) / ? * ;,: % . _ - " " "
+
+all=lower + upper +NUMBER + Symbol
+lenght = 9
+password = "" .join (random.sample(all, lenght))
+print("You were Generate The password :", password)
+
+ 
---
+++ hi.py
@@ -0,0 +1,9 @@
+from turtle import *
+speed(11)
+color('cyan')
+bgcolor('black')
+y = 200
+while y > 0 :
+        left(y)
+        forward(y *3)
+        y = y-1
